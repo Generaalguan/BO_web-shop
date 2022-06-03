d9b1f272d8fe3d595d82d4cf82d994648cbcee66
Generaalguan <33372@ma-web.nl>
2022-06-03 11:59:15 +0200

    Logo-design

--- index.php
+++ index.php
@@ -18,10 +18,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
-    </style>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time()?>">
     <title>Web-shop</title>
 </head>
 
@@ -30,7 +27,7 @@
         <div class="container">
 
             <div class="navbar">
-                <a class="logo" href="index.html"><img src="img/noBorder.png" alt="" width="145rem"></a>
+                <a class="logo" href="index.php"><img src="img/noBorder.png" alt="" width="145rem"></a>
                 <nav class="nav">
                     <ul id="menuItems">
                         <li><a href="">Home</a></li>
@@ -46,9 +43,9 @@
 
             <div class="row">
                 <div class="col-2">
-                    <h1>Find the perfect person <br> for your business!</h1>
+                    <h1>Find the perfect person, <br> for your business!</h1>
                     <p>OpenHire aims to make it easier for businesses to look for freelancers they can hire for their
-                        projects,</p>
+                        projects</p>
                     <a href="" class="btn">Explore Now &#8594;</a>
                 </div>
                 <div class="col-2"><img src="img/laptop.png" alt=""></div>
@@ -74,20 +71,20 @@
             <button class="arrow"><</button>
                     <ul class="reviews">
 
-                            <li class="review">
-                                <a href="">
-                                    <img src="img/logo-design-2.webp" width="100%" alt="">
-                                    <div class="cat-1">
-                                        <h3>Logo design</h3>
-                                        <p>Let us make a logo for you</p>
-                                    </div>
+                        <li class="review">
+                            <a href="logo-design.html" target="_blank">
+                                <img src="img/logo-design-2.webp" width="100%" alt="">
+                                <div class="cat-1">
+                                    <h3>Logo design</h3>
+                                    <p>Let us make a logo for you</p>
+                                </div>
                             </a>
                         </li>
 
                         
 
                         <li class="review">
-                            <a href="">
+                            <a href="translation.html" target="_blank">
                                 <img src="img/translation-2x.webp" width="100%" alt="">
                                 <div class="cat-1">
                                     <h3>Backend developer</h3>
@@ -99,7 +96,7 @@
 
                         
                         <li class="review">
-                            <a href=""></a>
+                            <a href="web-developer.html" target="_blank">
                                 <img src="img/seo-2x.webp" width="100%" alt="">
                                 <div class="cat-1">
                                     <h3>Web developer</h3>
@@ -111,7 +108,7 @@
 
                         
                         <li class="review">
-                            <a href=""></a>
+                            <a href="wordpress.html" target="_blank">
                                 <img src="img/wordpress-2x.webp" width="100%" alt="">
                                 <div class="cat-1">
                                     <h3>Web design</h3>
@@ -123,7 +120,7 @@
 
 
                             <li class="review">
-                                <a href="">
+                                <a href="illustration.html" target="_blank">
                                     <img src="img/illustration-2x.webp" width="100%" alt="">
                                     <div class="cat-1">
                                         <h3>illustrations</h3>
@@ -135,7 +132,7 @@
 
                     
                         <li class="review">
-                            <a href=""></a>
+                            <a href="data-entry.html" target="_blank">
                                 <img src="img/data-entry-2x.webp" width="100%" alt="">
                                 <div class="cat-1">
                                     <h3>Data Gathering</h3>
@@ -148,8 +145,8 @@
                     <button class="arrow">></button>
         </section>
         <!--Programmers-->
-        <h2 class="titel">Programmers</h2>
-
+        <div class="title_padding"><h2 class="titel">Our top Developpers</h2></div>
+        
         <section class="small-container">
             <div class="row">
                 <div class="col-4">
@@ -157,7 +154,7 @@
                         <img src="img/Dion.webp" class="img_dev"  alt="">
                         <div class="cat-2">
                             <h3>Dion ahiyabor</h3>
-                            <p>A pro in Web Design</p>
+                            <p>A pro in Design</p>
                         </div>
                     </a>
 
@@ -167,7 +164,7 @@
                         <img src="img/Bilal.webp" class="img_dev" alt="">
                         <div class="cat-2">
                             <h3>Bilal el Koudadi</h3>
-                            <p>A pro in Web Development</p>
+                            <p>A pro in Development</p>
                         </div>
                     </a>
 
@@ -200,7 +197,7 @@
                     <?php foreach($result as $row): ?>
                         <div class="col-3">
                             <i class="fa fa-quote-left"></i>
-                            <p class="text"><?php echo $row['beschrijfing'] ?></p>
+                            <p class="text"><?php echo $row['beschrijving'] ?></p>
                             
                             <figure class="programmers-img" style="background-image: url(/f2m3bap/Web-shop/img/<?php echo $row['foto'];?>)" > </figure>
                             <h3><?php echo $row['titel']?></h3>
