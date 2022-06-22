e29d2084e91c3326f9ae83020a86189b45592778
Generaalguan <33372@ma-web.nl>
2022-06-22 09:28:37 +0200

    error_email

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
                     <ul id="menuItems">
                         <li><a href="">About</a></li>
                         <li><a href="">Account</a></li>
-                        <li><a class="cta" href="contact.php">Contact</a></li>
+                        <li><a class="cta" target="blanck" href="contact.php">Contact</a></li>
                     </ul>
                 </nav>
 
